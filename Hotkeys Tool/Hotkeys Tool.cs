e4926f89a0d148e0f15b5805055a6e91f6cc28b1
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-08-31 10:58:34 +0300

    Added chart controls

--- Hotkeys Tool/Hotkeys Tool.cs
+++ Hotkeys Tool/Hotkeys Tool.cs
@@ -67,6 +67,30 @@ namespace cAlgo.Robots
         [Parameter("Cancel All Stop Limit Key", DefaultValue = Key.V, Group = "Trading")]
         public Key CancelAllStopLimitKey { get; set; }
 
+        [Parameter("Horizontal Alignment", DefaultValue = HorizontalAlignment.Right, Group = "Chart Controls")]
+        public HorizontalAlignment HorizontalAlignment { get; set; }
+
+        [Parameter("Vertical Alignment", DefaultValue = VerticalAlignment.Top, Group = "Chart Controls")]
+        public VerticalAlignment VerticalAlignment { get; set; }
+
+        [Parameter("Background Color", DefaultValue = "Yellow", Group = "Chart Controls")]
+        public string BackgroundColor { get; set; }
+
+        [Parameter("Text Color", DefaultValue = "Black", Group = "Chart Controls")]
+        public string TextColor { get; set; }
+
+        [Parameter("Opacity", DefaultValue = 0.6, MinValue = 0, MaxValue = 1, Group = "Chart Controls")]
+        public double Opacity { get; set; }
+
+        [Parameter("Margin", DefaultValue = 5, MinValue = 0, Group = "Chart Controls")]
+        public double Margin { get; set; }
+
+        [Parameter("Font Size", DefaultValue = 14, MinValue = 0, Group = "Chart Controls")]
+        public double FontSize { get; set; }
+
+        [Parameter("Font Weight", DefaultValue = FontWeight.Normal, Group = "Chart Controls")]
+        public FontWeight FontWeight { get; set; }
+
         protected override void OnStart()
         {
             if (StopLossInPips > 0) _stopLossInPips = StopLossInPips;
@@ -74,6 +98,73 @@ namespace cAlgo.Robots
 
             PendingOrderDistanceInPips *= Symbol.PipSize;
 
+            AddTradingHotkeys();
+
+            ShowHotkeysOnChart();
+        }
+
+        private void ShowHotkeysOnChart()
+        {
+            var grid = new Grid(13, 2)
+            {
+                HorizontalAlignment = HorizontalAlignment,
+                VerticalAlignment = VerticalAlignment,
+                Opacity = Opacity,
+                BackgroundColor = GetColor(BackgroundColor)
+            };
+
+            var textBlocksStyle = new Style();
+
+            textBlocksStyle.Set(ControlProperty.Margin, Margin);
+            textBlocksStyle.Set(ControlProperty.FontSize, FontSize);
+            textBlocksStyle.Set(ControlProperty.FontWeight, FontWeight);
+            textBlocksStyle.Set(ControlProperty.ForegroundColor, GetColor(TextColor));
+
+            var tradingHotkeysTextblock = new TextBlock { Text = "Trading", HorizontalAlignment = HorizontalAlignment.Center, Style = textBlocksStyle };
+
+            grid.AddChild(tradingHotkeysTextblock, 0, 0, 1, 2);
+
+            grid.AddChild(new TextBlock { Text = "Buy Market", Style = textBlocksStyle }, 1, 0);
+            grid.AddChild(new TextBlock { Text = GetHotkeyText(BuyMarketKey, TradingModifierKey), Style = textBlocksStyle }, 1, 1);
+
+            grid.AddChild(new TextBlock { Text = "Sell Market", Style = textBlocksStyle }, 2, 0);
+            grid.AddChild(new TextBlock { Text = GetHotkeyText(SellMarketKey, TradingModifierKey), Style = textBlocksStyle }, 2, 1);
+
+            grid.AddChild(new TextBlock { Text = "Close All Market", Style = textBlocksStyle }, 3, 0);
+            grid.AddChild(new TextBlock { Text = GetHotkeyText(CloseAllMarketKey, TradingModifierKey), Style = textBlocksStyle }, 3, 1);
+
+            grid.AddChild(new TextBlock { Text = "Buy Limit", Style = textBlocksStyle }, 4, 0);
+            grid.AddChild(new TextBlock { Text = GetHotkeyText(BuyLimitKey, TradingModifierKey), Style = textBlocksStyle }, 4, 1);
+
+            grid.AddChild(new TextBlock { Text = "Sell Limit", Style = textBlocksStyle }, 5, 0);
+            grid.AddChild(new TextBlock { Text = GetHotkeyText(SellLimitKey, TradingModifierKey), Style = textBlocksStyle }, 5, 1);
+
+            grid.AddChild(new TextBlock { Text = "Cancel All Limit", Style = textBlocksStyle }, 6, 0);
+            grid.AddChild(new TextBlock { Text = GetHotkeyText(CancelAllLimitKey, TradingModifierKey), Style = textBlocksStyle }, 6, 1);
+
+            grid.AddChild(new TextBlock { Text = "Buy Stop", Style = textBlocksStyle }, 7, 0);
+            grid.AddChild(new TextBlock { Text = GetHotkeyText(BuyStopKey, TradingModifierKey), Style = textBlocksStyle }, 7, 1);
+
+            grid.AddChild(new TextBlock { Text = "Sell Stop", Style = textBlocksStyle }, 8, 0);
+            grid.AddChild(new TextBlock { Text = GetHotkeyText(SellStopKey, TradingModifierKey), Style = textBlocksStyle }, 8, 1);
+
+            grid.AddChild(new TextBlock { Text = "Cancel All Stop", Style = textBlocksStyle }, 9, 0);
+            grid.AddChild(new TextBlock { Text = GetHotkeyText(CancelAllStopKey, TradingModifierKey), Style = textBlocksStyle }, 9, 1);
+
+            grid.AddChild(new TextBlock { Text = "Buy Stop Limit", Style = textBlocksStyle }, 10, 0);
+            grid.AddChild(new TextBlock { Text = GetHotkeyText(BuyStopLimitKey, TradingModifierKey), Style = textBlocksStyle }, 10, 1);
+
+            grid.AddChild(new TextBlock { Text = "Sell Stop Limit", Style = textBlocksStyle }, 11, 0);
+            grid.AddChild(new TextBlock { Text = GetHotkeyText(SellStopLimitKey, TradingModifierKey), Style = textBlocksStyle }, 11, 1);
+
+            grid.AddChild(new TextBlock { Text = "Cancel All Stop Limit", Style = textBlocksStyle }, 12, 0);
+            grid.AddChild(new TextBlock { Text = GetHotkeyText(CancelAllStopLimitKey, TradingModifierKey), Style = textBlocksStyle }, 12, 1);
+
+            Chart.AddControl(grid);
+        }
+
+        private void AddTradingHotkeys()
+        {
             Chart.AddHotkey(() => MarketOrderKeyHandler(TradeType.Buy), BuyMarketKey, TradingModifierKey);
             Chart.AddHotkey(() => MarketOrderKeyHandler(TradeType.Sell), SellMarketKey, TradingModifierKey);
             Chart.AddHotkey(CloseMarketOrders, CloseAllMarketKey, TradingModifierKey);
@@ -139,6 +230,18 @@ namespace cAlgo.Robots
         private void MarketOrderKeyHandler(TradeType tradeType)
         {
             ExecuteMarketOrderAsync(tradeType, SymbolName, Volume, Label, _stopLossInPips, _takeProfitInPips);
+        }
+
+        private Color GetColor(string colorString, int alpha = 255)
+        {
+            var color = colorString[0] == '#' ? Color.FromHex(colorString) : Color.FromName(colorString);
+
+            return Color.FromArgb(alpha, color);
+        }
+
+        private string GetHotkeyText(Key key, ModifierKeys modifier)
+        {
+            return modifier == ModifierKeys.None ? key.ToString() : string.Format("{0}+{1}", key, modifier);
         }
     }
 }
